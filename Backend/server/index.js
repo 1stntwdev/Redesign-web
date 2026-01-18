c10e9f6c4becae5491460e071148ce5a7e4f866d
1stntwdev <1stntw.dev@gmail.com>
2026-01-18 20:56:54 +0700

    refactor connection DB
    change to async function

--- Backend/server/index.js
+++ Backend/server/index.js
@@ -25,26 +25,28 @@ app.post('/upload', upload.single('photo'), (req, res) => {
 })
 require('dotenv').config();
 const {MYSQL_HOST,MYSQL_USER,MYSQL_PWD,MYSQL_DB} = process.env
-// insert img ------------------------
-app.get('/testdb', (req, res) => {
-  mysql.createConnection({
-    host:MYSQL_HOST,
-    user:MYSQL_USER,
-    password:MYSQL_PWD,
-    database:MYSQL_DB,
-  }).then((conn) => {
-    // สิ่งนี้เราเรียกกันว่า promise
-    conn
-    .query('SELECT * FROM product_plant')
-    .then((results) => {
-      res.json(results[0])
-    })
-    .catch((error) => {
-      console.error('Error fetching users:', error.message)
-      res.status(500).json({ error: 'Error fetching users' })
-    })
+// insert end img ------------------------
+
+let conn = null;
+const initMySQL = async()=>{
+  conn = await mysql.createConnection({
+      host:MYSQL_HOST,
+      user:MYSQL_USER,
+      password:MYSQL_PWD,
+      database:MYSQL_DB,
+    });
+}
+app.get('/testdb-new',async (req,res)=>{
+  try{
+    const result = await conn.query('select * from product_plant');
+    res.json(result[0]);
+  }
+  catch (error){
+    console.error(error.message);
+    res.status(500).json({error:"Error query fetching "})
+  }
   })
-})
-app.listen(port, () => {
+app.listen(port, async() => {
+  await initMySQL();
   console.log(`Example app listening on port ${port}`)
 })
