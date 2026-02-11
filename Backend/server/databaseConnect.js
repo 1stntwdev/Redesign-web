f9d3b821eb23d98d8e3c854a3f07283ddc6fe165
1stntwdev <1stntw.dev@gmail.com>
2026-02-12 02:51:33 +0700

    Fix : if server not turn on log error
    getConnection() and release(); are method of pool

--- Backend/server/databaseConnect.js
+++ Backend/server/databaseConnect.js
@@ -7,13 +7,21 @@ const { MYSQL_HOST, MYSQL_USER, MYSQL_PWD, MYSQL_DB } = process.env;
 let pool;
 
 export const initMySQL = async () => {
-    pool = mysql.createPool({
-        host: MYSQL_HOST,
-        user: MYSQL_USER,
-        password: MYSQL_PWD,
-        database: MYSQL_DB,
-    });
-    console.log('✅ MySQL connected');
+    try {
+        pool = mysql.createPool({
+            host: MYSQL_HOST,
+            user: MYSQL_USER,
+            password: MYSQL_PWD,
+            database: MYSQL_DB,
+        });
+        const connection = await pool.getConnection();
+        connection.release();
+        console.log('✅ MySQL connected');
+        
+    } catch (error) {
+     console.log(`❌ MySQL connecting failed , ${error.message}`);
+    }
+   
 };
 
 export const getConn = () => {
