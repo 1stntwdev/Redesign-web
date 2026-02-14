c6246c0b2bdf08873018c15aaa9ccd578c53866b
1stntwdev <1stntw.dev@gmail.com>
2026-02-14 09:31:04 +0700

    feat : get id
    get id from URLSearchParams.

--- Frontend/src/pages/user/Product/product.js
+++ Frontend/src/pages/user/Product/product.js
@@ -2,24 +2,19 @@
 export const components = {
   loadNav: async () => {
     try {
-      console.log("LOAD NAV RUNNING");
       const navElement = document.getElementById("nav-container");
       const response = await axios.get("/Frontend/src/Components/Nav/nav.html");
       navElement.innerHTML = response.data;
       addToggleNav()
       addStyleNav();
       
-        // initLoginRegister();
-      
     } catch (error) {
       console.error(`somethinh erro loadNav`, error)
     }
-
   }
 }
 const addToggleNav = () => {
   const js = [
-   
     "/Frontend/src/assets/js/modalLogin.js",
     "/Frontend/src/assets/js/logout.js",
      "/Frontend/src/assets/js/toggleHome.js"
@@ -30,7 +25,6 @@ const addToggleNav = () => {
     script.defer = true;
     document.body.appendChild(script);
   });
-
 }
 function addStyleNav() {
   const link = document.createElement("link");
@@ -44,3 +38,13 @@ function addStyleNav() {
   document.head.appendChild(link);
   document.head.appendChild(linkModal);
 }
+
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+async function searchProduct(id){
+  const response = await axios.get(`http://localhost:8000/api/product_id/${id}`)
+  const data = response.data;
+}
+  
