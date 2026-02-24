c4df968485689479bd3928d8d9bbf634fb50dbaf
1stntwdev <1stntw.dev@gmail.com>
2026-02-25 03:52:42 +0700

    refactor : attribute
    add feat count qty in cart.

--- Frontend/src/pages/user/Product/product.js
+++ Frontend/src/pages/user/Product/product.js
@@ -1,3 +1,5 @@
+import {componentCart} from '/Frontend/src/assets/js/addTocart.js';
+
 const addToggleNav = () => {
   const js = [
     "/Frontend/src/assets/js/modalLogin.js",
@@ -11,22 +13,23 @@ const addToggleNav = () => {
     document.body.appendChild(script);
   });
 }
-
-
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
+const componentnElement = {
+  "img" : document.getElementById("plant-img"),
+  "name" : document.querySelector(".product-title"),
+  "price" : document.querySelector(".product-price"),
+   "description" : document.querySelector(".product-description"),
+ "category" : document.querySelector(".product-category"),
+ "wide" : document.querySelector(".product-wide"),
+ "height" : document.querySelector(".product-high"),
+ "amount" : document.querySelector(".product-amount"),
 
-const imgElement = document.getElementById("plant-img");
-const nameElement = document.querySelector(".product-title");
-const priceElement = document.querySelector(".product-price");
-const descriptionElement = document.querySelector(".product-description");
-const categoryElement = document.querySelector(".product-category");
-const wideElement = document.querySelector(".product-wide");
-const highElement = document.querySelector(".product-high");
-const amountElement = document.querySelector(".product-amount");
+}
 const btn = document.createElement('button');
 const qtyArea = document.querySelector('.quantity-area');
 const textArea = document.querySelector('.text-area')
+
 export async function searchProduct() {
   try {
     const response = await axios.get(`http://localhost:8000/api/product_id/${id}`)
@@ -37,15 +40,18 @@ export async function searchProduct() {
     const {
       name, price, amount, description, img, light_type_id: category, wide, high
     } = response.data;
-    imgElement.src = "/Backend/server/uploads/" + img;
-    nameElement.textContent = name;
-    priceElement.textContent = price + "THB";
-    descriptionElement.textContent = description;
-    categoryElement.textContent = category;
-    wideElement.textContent = wide + `"`;
-    highElement.textContent = high + `"`;
-    amountElement.textContent = amount + ` remaining`;
+    qtyArea.dataset.stock = amount;
+    componentnElement.img.src = "/Backend/server/uploads/" + img;
+    componentnElement.name.textContent = name;
+    componentnElement.price.textContent = price + "THB";
+    componentnElement.description.textContent = description;
+    componentnElement.category.textContent = category;
+    componentnElement.wide.textContent = wide + `"`;
+    componentnElement.height.textContent = high + `"`;
+    componentnElement.amount.textContent = amount + ` remaining`;
     qtyArea.appendChild(btn);
+    componentCart.qtyControl()
+    
   } catch (error) {
       console.log(`error`+error)
   }
@@ -56,8 +62,8 @@ export async function searchProduct() {
 textArea.addEventListener('click',(e)=>{
   const btn = e.target.closest('.add-cart');
   if(!btn) return;
-  console.log(`id=`+id)
-    componentCart.addTocart(id,btn.dataset.stock);
+  const qty = Number(document.querySelector('.qty-number').value);
+  componentCart.productAdd(id,qty);
 })
 
-import {componentCart} from '/Frontend/src/assets/js/addTocart.js';
+
