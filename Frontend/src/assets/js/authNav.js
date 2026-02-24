2520dbe66bb53695236935307c7ae741f7765e5f
1stntwdev <1stntw.dev@gmail.com>
2026-02-25 03:57:52 +0700

    add : set number cart update

--- Frontend/src/assets/js/authNav.js
+++ Frontend/src/assets/js/authNav.js
@@ -1,21 +1,25 @@
-// import { loadNav } from '/Frontend/src/Components/Nav/nav.js';
 import { initLoginRegister } from '/Frontend/src/assets/js/loginRegister.js';
+import { componentCart } from '/Frontend/src/assets/js/addTocart.js'
+
 export async function initAuthNav() {
   const token = localStorage.getItem("token");
   if (!token) {
-    loadNav();
+    await loadNav();
+    componentCart.setStyle();
     initLoginRegister();
     return;
   }
   console.log(token)
   await loadLoginNav();
+  componentCart.setStyle();
+
 }
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 export async function loadNav() {
   try {
     const response = await axios.get('/Frontend/src/Components/Nav/nav.html');
-   
+
     document.getElementById('nav-container').innerHTML = response.data;
     const cartNumber = document.querySelector(".cart-number");
     const toggleScript = document.createElement('script');
@@ -25,7 +29,7 @@ export async function loadNav() {
     const modalScript = document.createElement('script');
     modalScript.src = "/Frontend/src/assets/js/modalLogin.js";
     document.body.appendChild(modalScript);
-    if(cart.length > 0){
+    if (cart.length > 0) {
       cartNumber.style.display = "flex";
       cartNumber.textContent = cart.length;
     }
@@ -52,7 +56,7 @@ async function loadLoginNav() {
     toggleScript.src = "/Frontend/src/assets/js/toggleHome.js";
     document.body.appendChild(toggleScript);
     const cartNumber = document.querySelector(".cart-number");
-    if(cart.length > 0){
+    if (cart.length > 0) {
       cartNumber.style.display = "flex";
       cartNumber.textContent = cart.length;
     }
